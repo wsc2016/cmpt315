4a5daa7857a14269116845797b4e2c92c621c445
Wilson Ho <who@ualberta.ca>
2016-03-13 16:23:26 -0600

    add stormpath and basic authentication functionalities

---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+var routes = require('./lib/routes');
+
+/**
+ * Create the Express application.
+ */
+var app = express();
+
+/**
+ * Application settings.
+ */
+app.set('trust proxy',true);
+app.set('view engine', 'jade');
+app.set('views', './lib/views');
+app.locals.siteName = 'Collaborama';
+
+/**
+ * Stormpath initialization.
+ */
+
+console.log('Initializing Stormpath');
+
+app.use(stormpath.init(app, {
+  expand: {
+    customData: true
+  }
+}));
+
+/**
+ * Route initialization.
+ */
+app.use('/', routes);
+
+app.on('stormpath.ready',function () {
+  console.log('Stormpath Ready');
+});
+
+/**
+ * Start the web server.
+ */
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Server listening on http://localhost:' + port);
+});
+
