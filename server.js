ce90ea583e1843a1a2e891118ec3ee279161414c
Wilson Ho <who@ualberta.ca>
2016-03-24 16:18:51 -0600

    callout form and other dependencies

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var stormpath = require('express-stormpath');
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
+var path = require('path');
+
 
 var callouts = require('./lib/modules/callouts');
 var routes = require('./lib/routes');
@@ -25,7 +31,8 @@ var app = express();
  */
 app.set('trust proxy',true);
 app.set('view engine', 'jade');
-app.set('views', './lib/views');
+// app.set('views', './lib/views');
+app.set('views', path.join(__dirname, './lib/views'));
 app.locals.siteName = 'Collaborama';
 
 // API routes
@@ -63,13 +70,47 @@ app.use(stormpath.init(app, {
 /**
  * Route initialization.
  */
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'lib')));
 app.use('/', routes);
 
 app.on('stormpath.ready',function () {
   console.log('Stormpath Ready');
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 /**
  * Start the web server.
  */
